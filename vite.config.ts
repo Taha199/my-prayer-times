9f1445a4c0fb4158bd48c09bb316d61c9baa4a5b
Abdullah Taha <abdullahtaha@MacBook-Pro-som-tillhor-Abdullah.local>
2025-12-14 04:16:06 +0100

    fix vite env config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-});
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    define: {
+      __GEMINI_API_KEY__: JSON.stringify(env.GEMINI_API_KEY),
+    },
+  }
+})
